c54b189acb8a20970a0e255bf07e280c982fe23c
imbajul <doberstein-julian@web.de>
2024-01-26 14:44:42 +0100

    add Backend and Frontend

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,11 +1,11 @@
-import Item from "./components/Item.js"
-import Button from "./components/Button.js";
 import Calculator from "./components/Calculator.js"
 
 function App() {
   return (
     <div className="App">
-     <Calculator />
+      <Calculator />
     </div>
   );
 }
+
+export default App;
